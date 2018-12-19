859ae21b75b487379f7e3b25e2656f4b93383d5f
amr omar <a.ahmed@hyvetechnology.com>
2018-12-19 14:40:39 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
+//class is stateful
 class App extends Component {
+
+  state = {
+    persons: [
+      {id:'ssdsdad', name: 'amr', age: '25' },
+      {id:'sdada', name: 'omar', age: '27' },
+      {id:'sfasfdf', name: 'ahmed', age: '30' },
+    ],
+    showPerson: true
+  }
+  showPersonsHandler = () => {
+    const doesShow = this.state.showPerson;
+    this.setState({ showPerson: !doesShow })
+  }
+  deletePersonHandler =(personIndex)=>{
+    // const persons= this.state.persons;
+    // persons.splice(personIndex,1);
+    const persons = [...this.state.persons];
+    persons.splice(personIndex,1)
+    this.setState({persons:persons})
+  }
+  changeNameHandler = (event,personId)=>{
+      const personIndex = this.state.persons.findIndex((pers)=>{
+          return pers.id === personId;
+      });
+      
+      const person = {...this.state.persons[personIndex]};
+      person.name=event.target.value;
+
+      const persons = [...this.state.persons];
+      persons[personIndex]= person;
+
+      this.setState({persons:persons})
+  }
   render() {
+    const styleBtn = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
+    let persons = null;
+
+    if (this.state.showPerson) {
+      persons = (
+        // <div>
+        //   < Person
+        //     name={this.state.persons[0].name}
+        //     age={this.state.persons[0].age} />
+        //   <Person
+        //     name={this.state.persons[1].name}
+        //     age={this.state.persons[1].age}>My Hobbies are playing football</Person>
+        //   <Person
+        //     name={this.state.persons[0].name}
+        //     age={this.state.persons[0].age}>My Hobbies are playing football</Person>
+        // </div>
+        <div>
+        {this.state.persons.map((person,index)=>{
+          return <Person 
+          click={()=> this.deletePersonHandler(index)}
+            name={person.name} 
+            age={person.age}
+            key={person.id}
+            changed={(event)=> this.changeNameHandler(event,person.id)}
+
+            />
+        })}
+        </div>
+      );
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="App" >
+        <h1> Hello everyone from my react app</h1>
+        <button style={styleBtn} onClick={() => this.showPersonsHandler()}> Toggle persons</button >
+        {/* {this.state.showPerson === true ? 
+           <div>
+            < Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age} />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}>My Hobbies are playing football</Person>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}>My Hobbies are playing football</Person>
+          </div> 
+          : null} */}
+          
+          {persons}
+      </div >
     );
   }
 }
---
+++ src/Person/Person.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Person.css'
+
+// We can't use state here as we create the component as function 
+//so function is stateless
+const person =(props)=>{
+    return (
+        <div className="Person">
+            <p id='nameAge' onClick={props.click}>My name is <strong>{props.name}</strong> and my age is <strong>{props.age}</strong> </p>
+            <p>{props.children}</p>
+            {/* <input type="text" onChange={props.changed} value={props.name} /> */}
+            <input type="text" onChange={props.changed} placeholder={props.name} />
+        </div>
+    );
+}
+
+
+export default person;
